b7556b483d82191242a8eccb5dcc45e7ef174010
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-14 19:40:23 -0400

    Do things

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 const val blah: String = "good day lad!"
 
-fun main() {
+fun main() = runBlocking {
+    launch {
+        delay(10000L)
+        println("hi")
+    }
+    launch {
+        delay(1000)
+        print("I come second")
+    }
     println(blah)
 }
