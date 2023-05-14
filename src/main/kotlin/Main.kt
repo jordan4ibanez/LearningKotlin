82be077662b451aaf692af9ff250d535dd97eb6b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-14 19:59:21 -0400

    Now push it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun sum(a: Int, b: Int) = a + b
 
 fun main() = runBlocking {
     val blah: AtomicInteger = AtomicInteger(2)
-    repeat(50_000) {
+    repeat(5_000_000) {
         launch {
             if ((0..10_000).random() > 5_000) {
                 blah.incrementAndGet()
