7289c65ac162d6f613de195f0a890a1223f5ee10
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-14 21:14:11 -0400

    Be more expressive

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,20 +4,26 @@ import java.util.concurrent.atomic.AtomicInteger
 
 const val blah: String = "good day lad!"
 
-fun sum(a: Int, b: Int) = a + b
+fun sum(a: Number, b: Number) = a.toDouble().plus(b.toDouble()) as Number
 
-fun main() = runBlocking {
-    val blah: AtomicInteger = AtomicInteger(2)
-    repeat(5_000_000) {
-        launch {
-            if ((0..10_000).random() > 5_000) {
-                blah.incrementAndGet()
-                println("up $blah")
-            } else {
-                blah.decrementAndGet()
-                println("down $blah")
+fun main() = run {
+    val atomicCounter = AtomicInteger(2)
+    runBlocking {
+        repeat(50_000) {
+            launch {
+                if ((0..10_000).random() > 5_000) {
+                    atomicCounter.set(sum(atomicCounter.get(), 5).toInt())
+                } else {
+                    atomicCounter.decrementAndGet()
+                }
             }
         }
     }
-    println(blah)
+    println("final output: $atomicCounter")
+
+    val yeah = 5.30f
+
+    var test = sum(yeah, yeah)
+
+    println(test)
 }
