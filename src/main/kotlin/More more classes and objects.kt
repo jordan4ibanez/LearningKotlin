61657d2b13f1971df0368bcf7860594451e5f8e8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-26 22:12:03 -0400

    Fancy

--- src/main/kotlin/More more classes and objects.kt
+++ src/main/kotlin/More more classes and objects.kt
@@ -1,10 +1,45 @@
 fun noIdeaWhatToCallThis() {
-
+    val myCoolSentence = mutableListOf("hi", "there")
+    myCoolSentence.asSentence().also(::println)
+    myCoolSentence.asSentence(", ").also(::println)
+    myCoolSentence.swap(0,1)
+    myCoolSentence.asSentence(" ").also(::println)
+    // Added in defaults, so you can swap it as a pair
+    myCoolSentence.swap()
+    myCoolSentence.asSentence().also(::println)
+    // It's also chainable
+    myCoolSentence.swap().asSentence().also(::println)
 
 }
 
+// You can just bolt on stuff to anything, it's a miracle
+fun MutableList<String>.asSentence(separator: String = " "): String {
+    val builder = StringBuilder()
+    val words = this.size
+    this.forEachIndexed { key,value ->
+        builder.append(value)
+        if (key + 1 < words) builder.append(separator)
+    }
+    return builder.toString()
+}
+// Pure generics
+fun <T> MutableList<T>.swap(index1: Int = 0, index2: Int = 1): MutableList<T> {
+    assert(this.size >= 2)
+    val temp: T = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
+    return this
+}
 
-// This part is just copied and pasted from the tutorial with IntelliJ coloring so it's easier to understand
+// This part is just copied and pasted from the tutorial with IntelliJ coloring, so it's easier to understand
+
+private fun foo() { } // visible inside example.kt
+
+public var bar: Int = 5 // property is visible everywhere
+    private set         // setter is visible only in example.kt
+
+internal val baz = 6    // visible inside the same module
+
 open class Outer2 {
     private val a = 1
     protected open val b = 2
