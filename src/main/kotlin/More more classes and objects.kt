890a81c26ec31a47249dc87c8fcb5e123e12f124
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-26 21:51:39 -0400

    Shovel this tutorial in

---
+++ src/main/kotlin/More more classes and objects.kt
@@ -0,0 +1,32 @@
+fun noIdeaWhatToCallThis() {
+
+
+}
+
+
+// This part is just copied and pasted from the tutorial with IntelliJ coloring so it's easier to understand
+open class Outer2 {
+    private val a = 1
+    protected open val b = 2
+    internal open val c = 3
+    val d = 4  // public by default
+
+    protected class Nested {
+        public val e: Int = 5
+    }
+}
+
+class Subclass : Outer2() {
+    // a is not visible
+    // b, c and d are visible
+    // Nested and e are visible
+
+    override val b = 5   // 'b' is protected
+    override val c = 7   // 'c' is internal
+}
+
+class Unrelated(o: Outer2) {
+    // o.a, o.b are not visible
+    // o.c and o.d are visible (same module)
+    // Outer.Nested is not visible, and Nested::e is not visible either
+}
