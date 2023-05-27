4d6a4686043bbc0c23379785eb82998ed29bedc7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-26 20:57:42 -0400

    How is kotlin even real

--- src/main/kotlin/More classes and objects.kt
+++ src/main/kotlin/More classes and objects.kt
@@ -52,8 +52,79 @@ fun evenMoreThingsAHHH() {
         doMath(45).also { println("${boolToSentenceModifier(it)}, ${isOrNot(it)} even") }
     }
 
+    // Utilizing the example below with lambda syntax
+    val myCoolPrinter = Printer { println("test") }
+    myCoolPrinter.print()
+
+    val myCoolMathBoi = MathBoi (
+        mather = {input ->
+            (input > 5).also { println("$input greater than 5? $it") }
+        }
+    )
+    myCoolMathBoi.doMath(34)
+
+    // Okay this is just kinda getting ridiculous at this point
+    val mathMaster = MathMasterClass()
+    mathMaster.addNewFunction("add",
+        newFunction = {
+            var accumulator = 0
+            it.forEach { value ->
+                accumulator += value
+            }
+            return@addNewFunction accumulator
+        }
+    )
+    mathMaster.calculate("add", 1, 2, 3, 4, 5).also { println("1 + 2 + 3 + 4 + 5 = $it") }
 
 }
+
+// Now this part is a ridiculous experiment, I want to see if a custom object can hold a custom instance of functions
+
+typealias MathGeneric = (inputWrapper: IntArray) -> Any
+class MathMasterClass {
+    private val mathFunctions: HashMap<String, MathGeneric> = HashMap()
+
+    // Trying to leverage functional style
+
+    // So we don't want this to crash, so we'll do a deflection style with this
+    fun addNewFunction(name: String, newFunction: MathGeneric) = when (check(name)) {
+        true -> println("$name already exists in MathMasterClass instance!")
+        false -> mathFunctions[name] = newFunction
+    }
+
+    fun calculate(name: String, vararg input: Int): Any = when (check(name)) {
+        true -> safeGet(name)(input)
+        false -> Nothing()
+    }
+
+    private fun check(input: String): Boolean = mathFunctions.contains(input)
+    private fun safeGet(name: String): ((IntArray) -> Any) = mathFunctions[name]!!
+}
+
+// Long story short it can
+
+
+// This is the typeAliased version of the experiment with SAM shown below
+
+typealias IntPredicate2 = (i: Int) -> Boolean
+class MathBoi(private val mather: IntPredicate2) {
+    fun doMath(input: Int): Boolean = mather(input)
+}
+
+
+// I'm going to attempt to explain this ... magic
+interface Printer {
+    fun print()
+}
+// This is utilizing reflection to migrate the interface into a class constructor.
+// So basically, functional Interface Object implements printer from supplied argument. This automatically constructs
+// the object to be instanced with the generified function. Somehow this is converting a generic object
+// into an instance of a Printer interface. FIXME: This was very difficult to understand, so this might not
+// FIXME: be 100% correct. todo: research more into this topic if a use for it is found!
+fun Printer(block: () -> Unit): Printer = object : Printer {
+    override fun print() = block()
+}
+
 
 // Okay so I got to the part with SAM interfaces, so I want to experiment with it here
 
