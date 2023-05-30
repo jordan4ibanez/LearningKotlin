fc4c81b13147d66ca32a8961cb0ab339e9068f6e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-30 13:43:10 -0400

    This is kinda neat

--- src/main/kotlin/More Garbage.kt
+++ src/main/kotlin/More Garbage.kt
@@ -9,8 +9,54 @@ fun beginMoreGarbage() {
 
     mysteryBoi.scream()
 
+    // This portion is probably going to NEVER be used anywhere in my programs but I'm gonna try it anyways
+
+    abstract class WhatEvenHappensHere {
+        abstract fun test()
+    }
+
+    fun manufactureAnObject(): WhatEvenHappensHere {
+        // See next portion for the actual useful version
+        return object : WhatEvenHappensHere() {
+            override fun test() = println("hi")
+        }
+    }
+
+    val manufacturer = manufactureAnObject()
+
+    manufacturer.test()
+
+    // Next portion
+
+    // O nuu we can't do this
+    // val blarf = What()
+
+    // So I guess we can do this
+    // I suppose this would be a local factory function
+    fun thingsAreGettingComplicated(): What {
+        // This part I can see being kinda useful
+        return object : What {
+            override fun blah() {
+                println("blah")
+            }
+        }
+    }
+
+    // Or this
+    val blarf = object : What {
+        override fun blah() {
+            println("MORE BLAH!")
+        }
+    }
+
+    val blarf2 = thingsAreGettingComplicated()
+
+    blarf.blah()
+    blarf2.blah()
     
 
+}
 
-
+interface What {
+    fun blah()
 }
