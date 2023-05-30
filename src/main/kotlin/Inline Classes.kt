bb37b640a827dc8c55ca1b13ad24544e4a0d6055
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-30 13:05:15 -0400

    Push more garbage

--- src/main/kotlin/Inline Classes.kt
+++ src/main/kotlin/Inline Classes.kt
@@ -1,10 +1,18 @@
+import java.lang.Thread.sleep
+import kotlin.random.Random
+
 fun inlineClasses() {
 
     val myCoolPassword = Password("definitely don't do this")
 
-
+    // I started watching this video https://youtu.be/jhQgHW2VI0o so I decided to try to do it in Kotlin
+    // for(i in 0 until 10_000)if(Random.nextBoolean()) print("/")else print("\\")
+    
 
 }
+
+
+
 
 @JvmInline
 value class Password(private val s: String)
