20fbabd61da33c27b186e7f6e589359b1e9918ad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-31 16:42:58 -0400

    This is something else

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -19,6 +19,48 @@ fun timeForFunctions(input: String) {
     val (x,y,z) = arrayOf("x1", "y2", "z3")
 
     println("$x,$y,$z")
+
+    manyInput("hi", "there", "this", "is", "a", "test")
+
+    // Very readable, custom syntax, so neat
+    val isOneEqualToTwo = 1 compare 2
+
+    // This feels like BASIC
+    val test = (1 compare 2) then stage1@{
+        println("custom syntax result: $it")
+        return@stage1 if (it as Boolean) 1 else 0
+    } then stage2@{
+        when (it == 1) {
+            true -> println("Yeah m8")
+            false -> println("Nah m8")
+        }
+        println("$it next stage")
+
+        return@stage2 "How is this even possible?"
+    } with Pair(" 123") stage3@{ thenOutput, pairInput ->
+        if (thenOutput !is String || pairInput !is String) {
+            return@stage3 Nothing()
+        }
+        return@stage3 thenOutput + pairInput
+    }
+
+    println("The result of this experiment is: ($test)")
+
+}
+
+// You could probably turn this language into Lisp or some sort of Haskell thing
+infix fun Any.then(function: (Any) -> Any) = function(this)
+infix fun Any.with(functionData: Pair<Any, (Any, Any) -> Any>) = functionData.second(this, functionData.first)
+
+// That's so sick
+infix fun Any.compare(any: Any): Boolean {
+    return this == any
+}
+
+fun manyInput(vararg input: String) {
+    input.forEachIndexed { key,value ->
+        println("$key -> $value")
+    }
 }
 
 fun foo(
