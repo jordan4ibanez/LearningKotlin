6a56f75417ee5e770ad15bbf4eddf1c5e171c54f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-06-04 09:52:21 -0400

    Update Async.kt

--- src/main/kotlin/Async.kt
+++ src/main/kotlin/Async.kt
@@ -1,3 +1,69 @@
-fun doSomeAsync() {
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
 
+// This is a runBlocking scope.
+// You could use this in Main to have full access to coroutines across the entirety of your program.
+fun doSomeAsync() = runBlocking {
+
+    // This becomes a lightweight non-blocking thread
+    launch {
+        delay(10000)
+        println("This was executed first!")
+    }
+    println("This was executed second!")
+
+    // So does this
+    (0..1000).forEach { i ->
+        launch {
+            delay(Random.nextLong(10_000))
+//            println("hi $i")
+        }
+    }
+
+    println("This was executed last")
+
+    println("BEGIN NEXT EXECUTION")
+
+    launch { testIt() }
+
+
+    // Let's see if we can create an access violation
+
+    // ArrayList<String>() is direct version of mutableListOf<String>()
+    val myCoolList = mutableListOf<String>()
+
+    (0..1_000_000).forEach { _ ->
+
+        // This was a good test
+        launch {
+            delay(Random.nextLong(2))
+            StringBuilder().also { builder ->
+                for (i in 0..20) {
+                    builder.append(Random.nextInt(255).toChar())
+                }
+                println("appending ${builder.toString()}")
+                myCoolList.add(builder.toString())
+            }
+        }
+
+        // But let's make it harder
+        launch {
+            delay(Random.nextLong(2))
+
+            // The List might be empty, this is completely random
+            if (myCoolList.isEmpty()) return@launch
+
+            val gotten = myCoolList[Random.nextInt(myCoolList.size)]
+            println("retrieving $gotten")
+        }
+    }
+
+}
+
+// Suspend functions are pretty neat, useful
+suspend fun testIt() {
+    delay(10000L)
+    println("I am alive woo")
 }
